b1b544785a0317dd6fbb631eb88144b35394a788
Magdalena <magdalena.a.broniszewska@gmail.com>
2023-01-03 13:24:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const navList = document.querySelector(".main-nav-list");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+	const visibility = navList.getAttribute("data-visible");
+
+	if (visibility === "false") {
+		navList.setAttribute("data-visible", true);
+		navToggle.style.backgroundImage =
+			'url("../assets/images/icon-menu-close.svg")';
+	} else if (visibility === "true") {
+		navList.setAttribute("data-visible", false);
+		navToggle.style.backgroundImage = 'url("../assets/images/icon-menu.svg")';
+	}
+});
